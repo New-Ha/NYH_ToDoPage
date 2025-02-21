536533f7aecbdccba100d0394e6661d56f0a5436
UHa <https00200@gmail.com>
2025-02-22 04:36:04 +0900

    🚀 [Feat]: Set home page to the first subject page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,20 @@
-import BoardList from "@/components/board/BoardList";
-import Icon from "@/components/UI/Icon";
-import React from "react";
+"use client";
+
+import { useKanban } from "@/context/kanbanProvider";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const HomePage = () => {
-  return (
-    <div className="w-full flex flex-col items-center flex-1 min-h-0 px-2">
-      <div className="w-full max-w-[80rem] flex items-center justify-between my-10">
-        <div className="flex flex-row gap-2">
-          <button
-            type="button"
-            className="py-2 px-2 rounded-full hover:bg-border"
-          >
-            <Icon type="undo" className="w-5 h-5" />
-          </button>
-          <button
-            type="button"
-            className="py-2 px-2 rounded-full hover:bg-border"
-          >
-            <Icon type="redo" className="w-5 h-5" />
-          </button>
-        </div>
-        <h1 className="font-bold text-primary text-xl text-[1.4rem]">
-          Subject title
-        </h1>
-        <button
-          type="button"
-          className="py-2 px-2 rounded-full hover:bg-border"
-        >
-          <Icon type="dots-row" className="w-6 h-6" />
-        </button>
-      </div>
-      <div className="w-full max-w-[80rem] overflow-x-auto flex-1 min-h-0">
-        <BoardList />
-      </div>
-    </div>
-  );
+  const { kanbanData } = useKanban();
+  const router = useRouter();
+
+  useEffect(() => {
+    const subjectIds = Object.keys(kanbanData);
+    if (subjectIds.length > 0) {
+      router.replace(`/subject/${subjectIds[0]}`);
+  }, [kanbanData]);
+
+  return <p>로딩 중...</p>;
 };
 
 export default HomePage;
