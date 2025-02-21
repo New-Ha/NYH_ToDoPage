9491df80a27c0084a6561522d976082b2b3e5acb
UHa <https00200@gmail.com>
2025-02-22 02:30:48 +0900

    💄 [Design]: Modify homepage button icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,36 @@
 import BoardList from "@/components/board/BoardList";
+import Icon from "@/components/UI/Icon";
 import React from "react";
 
 const HomePage = () => {
   return (
-    <div className="w-full flex flex-col items-center flex-1 min-h-0">
-      {/* 내부 헤더 */}
-      <div className="w-full max-w-[80rem] flex items-center justify-between my-10 px-10">
-        <div>
-          <button type="button">👈🏻</button>
-          <button type="button">👉🏻</button>
+    <div className="w-full flex flex-col items-center flex-1 min-h-0 px-2">
+      <div className="w-full max-w-[80rem] flex items-center justify-between my-10">
+        <div className="flex flex-row gap-2">
+          <button
+            type="button"
+            className="py-2 px-2 rounded-full hover:bg-border"
+          >
+            <Icon type="undo" className="w-5 h-5" />
+          </button>
+          <button
+            type="button"
+            className="py-2 px-2 rounded-full hover:bg-border"
+          >
+            <Icon type="redo" className="w-5 h-5" />
+          </button>
         </div>
-        <h1 className="font-bold text-primary text-xl">주제명</h1>
-        <button type="button">...</button>
+        <h1 className="font-bold text-primary text-xl text-[1.4rem]">
+          Subject title
+        </h1>
+        <button
+          type="button"
+          className="py-2 px-2 rounded-full hover:bg-border"
+        >
+          <Icon type="dots-row" className="w-6 h-6" />
+        </button>
       </div>
-
-      <div className="w-full max-w-[80rem] overflow-x-auto flex-1 min-h-0 px-10">
+      <div className="w-full max-w-[80rem] overflow-x-auto flex-1 min-h-0">
         <BoardList />
       </div>
     </div>
