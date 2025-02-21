3ae2fe853d1460081bba63dae0977e8be6627311
UHa <https00200@gmail.com>
2025-02-22 04:33:44 +0900

    🚀 [Feat]:Modify subject list using context in header

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -1,14 +1,21 @@
+"use client";
+
 import React from "react";
 import SearchBar from "./SearchBar";
+import { useKanban } from "@/context/kanbanProvider";
 
 const Header = () => {
+  const { kanbanData } = useKanban();
+
   return (
     <header className="h-16 fixed top-0 left-0 right-0 border-b-[1px] border-border shadow-sm z-10">
       <div className="max-w-[80rem] mx-auto h-full flex items-center justify-between">
         <div className="flex items-center gap-4">
-          <h1 className="text-lg font-bold">Subject</h1>
-          <h1 className="text-lg font-bold">Subject</h1>
-          <h1 className="text-lg font-bold">Subject</h1>
+          {Object.entries(kanbanData).map(([subjectId, subject]) => (
+            <span key={subjectId} className="font-bold text-lg">
+              {subject.name}
+            </span>
+          ))}
           <button type="button">+</button>
         </div>
         <SearchBar />
--- src/types/kanban.type.ts
+++ src/types/kanban.type.ts
@@ -2,7 +2,7 @@ export type ToDoType = {
   id: string;
   boardId: string;
   content: string;
-  createdAt: string;
+  createdAt: Date;
 };
 
 export type BoardType = {
