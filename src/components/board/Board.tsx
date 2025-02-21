2e46e5ea12870aee98024464c59d5d712aeaed2c
UHa <https00200@gmail.com>
2025-02-22 02:27:40 +0900

    🚀 [Feat]: Add new task add button

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -5,7 +5,7 @@ import ToDoList from "../todo/ToDoList";
 const Board = () => {
   return (
     <section className="w-[20rem] bg-boardBG rounded-md border-border border-[1px]">
-      <div className="p-4 text-lg flex flex-row justify-between items-center border-b-[1px] border-[#C0C1CC] border-dashed">
+      <div className="h-[4rem] px-4 text-lg flex flex-row justify-between items-center border-b-[1px] border-[#C0C1CC] border-dashed">
         <p className="text-[1.2rem] text-primary font-bold px-2">
           board title
           <span className="px-2 py-1 ml-2 bg-border rounded-lg text-[1rem] font-thin">
@@ -14,13 +14,22 @@ const Board = () => {
         </p>
         <button type="button">
           <Icon
-            type="dots-column"
+            type="dots-row"
             className="w-[2.2rem] h-[2.2rem] py-1 px-2 rounded-xl hover:bg-border "
           />
         </button>
       </div>
       <div className="p-4">
         <ToDoList />
+      </div>
+      <div className="p-4 my-2">
+        <button
+          type="button"
+          className="flex flex-row justify-center items-center gap-2 w-full py-3 text-center border-border border-[1px] rounded-md hover:bg-white hover:shadow-sm"
+        >
+          <Icon type="plus-circle" className="w-5 h-5" />
+          <span className="text-[0.9rem]">New Task</span>
+        </button>
       </div>
     </section>
   );
