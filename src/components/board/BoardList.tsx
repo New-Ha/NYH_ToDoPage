aff2b598235a1f55a4e520a23df3c8475005f016
UHa <https00200@gmail.com>
2025-02-22 02:28:00 +0900

    🚀 [Feat]: Add board add button

--- src/components/board/BoardList.tsx
+++ src/components/board/BoardList.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Board from "./Board";
+import Icon from "../UI/Icon";
 
 const BoardList = () => {
   return (
-    <div className="flex gap-10 w-max">
-      <Board />
-      <Board />
-      <Board />
-      <Board />
-      <Board />
+    <div className="flex flex-row items-start gap-6">
+      <div className="flex gap-6 w-max">
+        <Board />
+        <Board />
+        <Board />
+      </div>
+      <button
+        type="button"
+        className="size-[4rem] flex justify-center items-center bg-boardBG rounded-md border-border border-[1px] hover:bg-border"
+      >
+        <Icon type="plus" />
+      </button>
     </div>
   );
 };
