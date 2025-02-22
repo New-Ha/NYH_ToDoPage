99b2298840c0cc49486e4c94d0a55c2971bf2f62
UHa <https00200@gmail.com>
2025-02-23 00:20:36 +0900

    🚀 [Feat]: Apply board context in BoardList

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,23 +1,89 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
 import Icon from "../UI/Icon";
 import ToDoList from "../todo/ToDoList";
+import { BoardType } from "@/types/kanban.type";
+import { useBoard } from "@/context/BoardProvider";
 
-const Board = () => {
+type BoardPropsType = {
+  subjectId: string;
+  board: BoardType;
+};
+
+const Board = ({ subjectId, board }: BoardPropsType) => {
+  const { deleteBoard, updateBoardTitle } = useBoard();
+  const [isEditBoard, setIsEditBoard] = useState(false);
+  const [isEditBoardName, setIsEditBoardName] = useState(false);
+  const [newBoardName, setNewBoardName] = useState("");
+
+  const handleEditBoardName = () => {
+    if (!newBoardName.trim()) {
+      setIsEditBoardName(false);
+      setIsEditBoard(false);
+      return;
+    }
+
+    updateBoardTitle(board.id, newBoardName);
+    setIsEditBoardName(false);
+    setIsEditBoard(false);
+  };
+
   return (
     <section className="w-[20rem] bg-boardBG rounded-md border-border border-[1px]">
       <div className="h-[4rem] px-4 text-lg flex flex-row justify-between items-center border-b-[1px] border-[#C0C1CC] border-dashed">
-        <p className="text-[1.2rem] text-primary font-bold px-2">
-          board title
-          <span className="px-2 py-1 ml-2 bg-border rounded-lg text-[1rem] font-thin">
-            2
-          </span>
-        </p>
-        <button type="button">
-          <Icon
-            type="dots-row"
-            className="w-[2.2rem] h-[2.2rem] py-1 px-2 rounded-xl hover:bg-border "
-          />
-        </button>
+        {isEditBoardName ? (
+          <div className="flex flex-row gap-2">
+            <input
+              type="text"
+              onChange={(e) => setNewBoardName(e.target.value)}
+              className="bg-transparent border-b-[1px] border-primary text-[1rem] font-semibold text-primary px-2 py-1 focus:outline-none"
+              placeholder={`${board.title}`}
+            />
+          </div>
+        ) : (
+          <p className="text-[1.2rem] text-primary font-bold px-2">
+            {board.title}
+            <span className="px-2 py-1 ml-2 bg-border rounded-lg text-[1rem] font-thin">
+              {board.todos.length}
+            </span>
+          </p>
+        )}
+        {isEditBoard ? (
+          <div>
+            {isEditBoardName ? (
+              <button
+                type="button"
+                onClick={handleEditBoardName}
+                className="px-2 py-2 rounded-full hover:bg-border"
+              >
+                <Icon type="check-circle" />
+              </button>
+            ) : (
+              <button
+                type="button"
+                onClick={() => setIsEditBoardName(true)}
+                className="py-2 px-2 rounded-full hover:bg-border"
+              >
+                <Icon type="pencil" />
+              </button>
+            )}
+            <button
+              type="button"
+              className="py-2 px-2 rounded-full hover:bg-border"
+              onClick={() => deleteBoard(subjectId, board.id)}
+            >
+              <Icon type="trash" />
+            </button>
+          </div>
+        ) : (
+          <button type="button" onClick={() => setIsEditBoard(true)}>
+            <Icon
+              type="dots-row"
+              className="w-[2.2rem] h-[2.2rem] py-1 px-2 rounded-xl hover:bg-border "
+            />
+          </button>
+        )}
       </div>
       <div className="p-4">
         <ToDoList />
--- src/components/board/BoardList.tsx
+++ src/components/board/BoardList.tsx
@@ -1,17 +1,36 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
 import Board from "./Board";
 import Icon from "../UI/Icon";
+import AddBoardForm from "./AddBoardForm";
+import { useBoard } from "@/context/BoardProvider";
 
-const BoardList = () => {
+type BoardListPropsType = {
+  subjectId: string;
+  boards: string[];
+};
+
+const BoardList = ({ subjectId }: BoardListPropsType) => {
+  const { boards: boardList } = useBoard();
+  const [isAddingBoard, setIsAddingBoard] = useState(false);
+
   return (
-    <div className="flex flex-row items-start gap-6">
+    <div className="w-max flex flex-row items-start gap-6">
       <div className="flex gap-6 w-max">
-        <Board />
-        <Board />
-        <Board />
+        {boardList.map((board) => (
+          <Board key={board.id} subjectId={subjectId} board={board} />
+        ))}
       </div>
+      {isAddingBoard ? (
+        <AddBoardForm
+          subjectId={subjectId}
+          onCancel={() => setIsAddingBoard(false)}
+        />
+      ) : null}
       <button
         type="button"
+        onClick={() => setIsAddingBoard(true)}
         className="size-[4rem] flex justify-center items-center bg-boardBG rounded-md border-border border-[1px] hover:bg-border"
       >
         <Icon type="plus" />
