64986dc17b24ef6689c74a1cfa0facabb36a22b1
UHa <https00200@gmail.com>
2025-02-25 01:45:28 +0900

    🚀 [Feat]: Add DndContext & SortableContext
    - board내에서 TodoItem이 드래그 앤 드롭으로 정렬 순서가 변경되도록 드래그 영역을 지정하고, 정렬 영역을 지정해줌
    - handleDragEnd 함수를 onDragEnd로 넘겨 드래그가 끝난 위치를 파악하고 인덱스가 바뀌도록 함
    - 위치가 바뀐 배열을 localStorage에 반영

--- src/components/todo/ToDoList.tsx
+++ src/components/todo/ToDoList.tsx
@@ -2,6 +2,12 @@
 
 import { useMemo } from "react";
 import { useTodo } from "@/context/TodoProvider";
+import { DndContext, DragEndEvent } from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 import ToDoItem from "./ToDoItem";
 
 const ToDoList = ({
@@ -18,16 +24,41 @@ const ToDoList = ({
     return getTodos(boardId);
   }, [boardId, todos]);
 
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+    if (!over || active.id === over.id) return;
+
+    const oldIndex = todoList.findIndex((todo) => todo.id === active.id);
+    const newIndex = todoList.findIndex((todo) => todo.id === over.id);
+
+    const newTodoList = arrayMove(todoList, oldIndex, newIndex);
+
+    const boardData = JSON.parse(
+      localStorage.getItem(`board_${boardId}`) || "null"
+    );
+    if (boardData) {
+      boardData.todos = newTodoList.map((todo) => todo.id);
+      localStorage.setItem(`board_${boardId}`, JSON.stringify(boardData));
+    }
+  };
+
   return (
-    <div className="flex flex-col gap-3">
-      {todoList.length === 0 && !isAddingTodo ? (
-        <div className="border-[1px] border-border rounded-md text-center text-grayText select-none py-3">
-          할 일을 등록해주세요.
+    <DndContext onDragEnd={handleDragEnd}>
+      <SortableContext
+        items={todoList.map((todo) => todo.id)}
+        strategy={verticalListSortingStrategy}
+      >
+        <div className="flex flex-col gap-3">
+          {todoList.length === 0 && !isAddingTodo ? (
+            <div className="border-[1px] border-border rounded-md text-center text-grayText select-none py-3">
+              할 일을 등록해주세요.
+            </div>
+          ) : (
+            todoList.map((todo) => <ToDoItem key={todo.id} todo={todo} />)
+          )}
         </div>
-      ) : (
-        todoList.map((todo) => <ToDoItem key={todo.id} todo={todo} />)
-      )}
-    </div>
+      </SortableContext>
+    </DndContext>
   );
 };
 
